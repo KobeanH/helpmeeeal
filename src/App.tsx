a770babaca8e972be7d558972d404bd9cca016a9
Hiro102520 <hirofumi.kobean102520@gmail.com>
2022-03-26 23:43:54 +0900

    log:周辺店舗検索結果typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
 import { VFC } from 'react';
+import { Top } from './components/pages/Top';
 
 const App: VFC = () => (
 
     <div className="App">
+      <Top/>
     </div>
   );
 
--- src/components/pages/Top.tsx
+++ src/components/pages/Top.tsx
@@ -1,9 +1,17 @@
 import { VFC } from 'react';
+import styled from 'styled-components';
+
+
 export const Top: VFC = () => (
 
 
 <>
-  <div id="map"></div>
+  <SMap id="map"></SMap>
   <p><button>近所のレストランを検索</button></p>
   </>
 );
+
+const SMap = styled.div`
+height:400px;
+width:600px;
+`
